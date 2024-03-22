3406fc50beff8562c89cdd6c04773196480ec285
CharLee Racine <charleeracine@yahoo.com>
2024-03-22 12:23:46 -0600

    Figuring out deleting child from parent.children when adding child to new parent

--- lib/tree_node.rb
+++ lib/tree_node.rb
@@ -7,8 +7,11 @@ class PolyTreeNode
   end
 
   attr_reader :value, :parent, :children
+  
+
 
   def parent=(parent_name)
+    
     old_parent = @parent
     
         
@@ -16,12 +19,35 @@ class PolyTreeNode
     unless parent_name == nil || parent.children.include?(self)
         @parent.children << self
     end
-    if old_parent && !old_parent.children.include?(self)
+
+    if old_parent
+    
         index = old_parent.children.index(self)
         
         old_parent.children = old_parent.children[0...index] + old_parent.children[index + 1..-1]
-        end
+    end
+
   end
+
+    # A is parent to B, B is child of A
+    def add_child(child_node)
+      child_node.parent = self
+    end
+
+    #A is parent to B. Remove B from A.  A.remove_child(B)
+
+    def remove_child(child_node)
+
+        if !self.children.include?(child_node)
+          raise "Not a parent"
+        end
+      
+        child_node.parent = nil
+    end
+
+   
+
+
 
   def inspect
     @value.inspect
